def2976ba050eb4fb7076ccff2e376d3f16124b2
Maick Souza <maick_a_s@msn.com>
2022-10-04 09:01:42 -0300

    test: do not create an appointment when date is before

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -2,12 +2,28 @@ import { expect, test } from "vitest";
 import { Appointment } from "./Appointment";
 
 test("create an appointment", () => {
+  const startsAt = new Date("2022-10-03T03:24:00");
+  const endsAt = new Date("2022-10-04T03:24:00");
+
   const appointment = new Appointment({
     customer: "John Doe",
-    startsAt: new Date(),
-    endsAt: new Date(),
+    startsAt,
+    endsAt,
   });
 
   expect(appointment).toBeInstanceOf(Appointment);
   expect(appointment.customer).toEqual("John Doe");
+});
+
+test("cannot create an appointment with end date before start date", () => {
+  const startsAt = new Date("2022-10-04T03:24:00");
+  const endsAt = new Date("2022-10-03T03:24:00");
+
+  expect(() => {
+    return new Appointment({
+      customer: "John Doe",
+      startsAt,
+      endsAt,
+    });
+  }).toThrow();
 });
